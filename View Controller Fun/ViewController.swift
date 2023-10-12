2056b605a4396c608faea615d2a94170542bde99
Annahlu Raclawski <annahlu47@gmail.com>
2023-10-12 09:17:01 -0500

    View Controller Notes

--- View Controller Fun/ViewController.swift
+++ View Controller Fun/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pony = ""
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //view.backgroundColor = UIColor.systemPink
+        print(pony)
+    }
 
-
+    @IBAction func manualButton(_ sender: UIButton) {
+        print("Hi")
+        
+        view.backgroundColor = UIColor.brown
+        pony = textFieldOutlet.text!
+        
+        performSegue(withIdentifier: "toRed", sender: self)
+    }
+    
+    //To add view controller click yellow button, editor, embed in, and navigation controller
+    //Once add view controller, add code
+    
+    //Manual segway, control click drag from yellow button to desired controller
+    
+    
+    //Automatically gets called just before a segue happens
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //nvc = next view controller giving access to the next view controller
+        let nvc = segue.destination as! ViewControllerAdmin
+        nvc.name = pony
+        nvc.delegate = self
+    }
+    
 }
 
